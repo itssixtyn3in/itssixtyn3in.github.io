de4ab593fe8170e91ce0e260b1f44e558eb4f34d
itssixtyn3in <130003354+itssixtyn3in@users.noreply.github.com>
2023-11-27 20:27:40 -0800

    Update index.js

--- payloads/securityportal/index.js
+++ payloads/securityportal/index.js
@@ -1,26 +1,24 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const fs = require('fs');
 
-const createWindow = () => {
-  const win = new BrowserWindow({
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 900,
+    backgroundColor: '#ffffff',
+    title: 'ElectroPwn',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      sandbox: false,
+      nodeIntegrationInSubFrames: false,
+      preload: path.join(__dirname, 'preload.js'),
+      nativeWindowOpen: true,
+      enableRemoteModule: false,
+      spellcheck: true
+    },
+  });
 
-  win.loadFile('index.html')
-}
-
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
