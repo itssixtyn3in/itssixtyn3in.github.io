f02614df45f011d959f22afba112e810329a983d
itssixtyn3in <130003354+itssixtyn3in@users.noreply.github.com>
2023-11-27 20:30:52 -0800

    Update index.js

--- payloads/securityportal/index.js
+++ payloads/securityportal/index.js
@@ -9,7 +9,6 @@ function createWindow() {
     width: 800,
     height: 900,
     backgroundColor: '#ffffff',
-    title: 'ElectroPwn',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -22,3 +21,27 @@ function createWindow() {
     },
   });
 
+  const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
