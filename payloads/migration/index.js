a37e92ce4e631f9b68946fbb9ce6f0822dc553c4
itssixtyn3in <130003354+itssixtyn3in@users.noreply.github.com>
2023-11-28 18:09:35 -0800

    Update index.js

--- payloads/migration/index.js
+++ payloads/migration/index.js
@@ -1,32 +1,37 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
 const fs = require('fs');
 
 let mainWindow;
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    frame: false, // Create a frameless window
-    backgroundColor: '#ffffff',
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      sandbox: false,
-      autoHideMenuBar: true,
-      nodeIntegrationInSubFrames: false,
-      preload: path.join(__dirname, 'preload.js'),
-      nativeWindowOpen: true,
-      enableRemoteModule: false,
-      spellcheck: true
-    },
-  });
-
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   mainWindow.loadFile('index.html');
 
-  // Rest of your Electron app initialization
-}
+  const filePath = 'C:\\Windows\\win.ini'; // Define the file path
 
-app.whenReady().then(createWindow);
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      // Handle error when file reading fails
+    } else {
+      // Send file content to the renderer process
+      mainWindow.webContents.send('file-content', data);
+    }
+  });
+});
+
+// Listen for renderer process requests for the file content
+ipcMain.on('get-file-content', (event) => {
+  // You can add more logic here if needed
+  // For example, re-read the file content or process it further
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      // Handle error when file reading fails
+    } else {
+      // Send file content to the renderer process
+      event.reply('file-content', data);
+    }
+  });
+});
